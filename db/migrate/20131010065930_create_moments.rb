623b7348c03e4808401ccb0f03384887bfaad426
Job van der Voort <jobvandervoort@gmail.com>
2013-10-19 23:26:19 +0200

    removed stuff

--- db/migrate/20131010065930_create_moments.rb
+++
@@ -1,9 +0,0 @@
-class CreateMoments < ActiveRecord::Migration
-  def change
-    create_table :moments do |t|
-      t.datetime :end_time
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20131010073245_add_timer_id_to_moments.rb
+++
@@ -1,7 +0,0 @@
-class AddTimerIdToMoments < ActiveRecord::Migration
-  def change
-    add_column :moments, :timer_id, :integer
-
-    add_index :moments, :timer_id
-  end
-end
