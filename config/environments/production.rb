95ecbb0a8caf21e64a24671524d4440b86fc7f82
Job van der Voort <jobvandervoort@gmail.com>
2013-11-02 16:07:58 +0100

    gem update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Jxtime::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.assets.initialize_on_precompile = false
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
