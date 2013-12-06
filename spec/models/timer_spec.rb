d2535a5208dc1443c2bda0c56c80f3fab480ba36
Job van der Voort <jobvandervoort@gmail.com>
2013-12-06 16:30:30 +0100

    add tests, better changelog

--- spec/models/timer_spec.rb
+++ spec/models/timer_spec.rb
@@ -7,4 +7,19 @@ describe Timer do
   ATTRIBUTES.each do |attr|
     specify { expect(timer).to respond_to(attr) }
   end
+
+  describe '.parse_time' do
+    context '2h' do
+      before { timer.written_time = '2h' }
+      specify { expect(timer.parse_time).to eq 7200 }
+    end
+    context '2m' do
+      before { timer.written_time = '2m' }
+      specify { expect(timer.parse_time).to eq 120 }
+    end
+    context '2s' do
+      before { timer.written_time = '2s' }
+      specify { expect(timer.parse_time).to eq 2 }
+    end
+  end
 end
