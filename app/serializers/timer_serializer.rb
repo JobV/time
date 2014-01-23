3715f9353cf29aabca99570c3b8eeb558ed1ef18
Job van der Voort <jobvandervoort@gmail.com>
2014-01-23 21:05:52 +0100

    total restyle

--- app/serializers/timer_serializer.rb
+++ app/serializers/timer_serializer.rb
@@ -2,4 +2,14 @@ class TimerSerializer < ActiveModel::Serializer
   attributes :id, :total_time, :created_at, :total_value, :start_time, :activity, :invoiced_at, :paid_at
   has_one :project
   has_one :client
+
+  # # TO BE TESTED
+  # def invoiced
+  #   object.invoiced_at ? true : false
+  # end
+
+  # # TO BE TESTED
+  # def paid_at
+  #   object.paid_at ? true : false
+  # end
 end
