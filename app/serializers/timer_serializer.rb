e0ab706210eed06d71a58e85b9fc662b050188c2
Job van der Voort <jobvandervoort@gmail.com>
2014-01-24 10:48:49 +0100

    style update and invoiced paid wip

--- app/serializers/timer_serializer.rb
+++ app/serializers/timer_serializer.rb
@@ -1,15 +1,23 @@
 class TimerSerializer < ActiveModel::Serializer
-  attributes :id, :total_time, :created_at, :total_value, :start_time, :activity, :invoiced_at, :paid_at
+  attributes :id, 
+    :total_time,
+    :created_at,
+    :total_value,
+    :start_time,
+    :activity,
+    :invoiced_at,
+    :paid_at
+
   has_one :project
   has_one :client
 
-  # # TO BE TESTED
-  # def invoiced
-  #   object.invoiced_at ? true : false
-  # end
+  # TO BE TESTED
+  def invoiced
+    object.invoiced_at ? true : false
+  end
 
-  # # TO BE TESTED
-  # def paid_at
-  #   object.paid_at ? true : false
-  # end
+  # TO BE TESTED
+  def paid
+    object.paid_at ? true : false
+  end
 end
