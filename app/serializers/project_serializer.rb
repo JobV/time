5bf6ad39f0f15d67cc485a4f02c8cb1e8a69125f
Job van der Voort <jobvandervoort@gmail.com>
2014-02-05 15:33:49 +0100

    style update

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,15 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :hourly_rate, :created_at
+  attributes :id, 
+    :name, 
+    :hourly_rate, 
+    :created_at,
+    :updated_at,
+    :uninvoiced,
+    :invoiced,
+    :paid,
+    :uninvoiced_time,
+    :invoiced_time,
+    :paid_time
+
   has_one :client
 end
